1ec40a736f16123459b74310f6b76caff285f16a
ishubhamjoshi <shubhamjoshi8890@gmail.com>
2025-03-09 01:10:09 +0530

    changes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,10 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('889094'),
-        ]);
+        // no changes to the default user
     }
 }
